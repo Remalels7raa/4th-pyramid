86a36c0b51bdeabaf1cce20b680d85ad591f0dfe
Asmaasamir20 <asmaasamir6699@gmail.com>
2025-01-19 18:52:37 +0200

    update

--- src/pages/components copy/Products/productsImages/index.js
+++
@@ -1,9 +0,0 @@
-// import { images1, images2, images3, images4, images5 } from "./productsImages";
-// import { images1 } from "./project1Images"
-// import { images2 } from "./project2Images"
-// import { images3 } from "./project3Images"
-// import { images4 } from "./project4Images"
-// import { images5 } from "./project5Images";
-
-
-// export { images1, images2, images3, images4, images5 };
